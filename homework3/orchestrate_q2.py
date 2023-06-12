ac6b0c727e66611058554a1efd58990c0327bb27
Roberto Vega <rober.vega93@gmail.com>
2023-06-12 10:04:53 +0100

    Remove some irrelevant spacing

--- homework3/orchestrate_q2.py
+++ homework3/orchestrate_q2.py
@@ -106,7 +106,6 @@ def train_best_model(
 
 
 @flow   
-
 def main_flow_q2(
     train_path: str = "homework3/green_tripdata_2023-01.parquet",
     val_path: str = "homework3/green_tripdata_2023-02.parquet",
